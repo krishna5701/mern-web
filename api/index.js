f8e75d7e3a30f21769aa90b84529a791726f4b54
kpatel43 <kpatel43@ncsu.edu>
2024-03-16 15:30:36 -0400

    Connected to MongoDB

--- api/index.js
+++ api/index.js
@@ -1,5 +1,16 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+dotenv.config();
 
+mongoose.connect(process.env.Mongodb)
+    .then (
+    () => {
+        console.log('Connected to DB');
+    })
+    .catch((err) => {
+        console.log(err);
+    })
 const app = express();
 
 app.listen(3000, () => {
